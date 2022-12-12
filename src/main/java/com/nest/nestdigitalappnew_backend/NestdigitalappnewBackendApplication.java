f1916f2d8aa3380dd605a904686151c78c372767
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-12-12 12:03:41 +0530

    initial

---
+++ src/main/java/com/nest/nestdigitalappnew_backend/NestdigitalappnewBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nest.nestdigitalappnew_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestdigitalappnewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestdigitalappnewBackendApplication.class, args);
+	}
+
+}
